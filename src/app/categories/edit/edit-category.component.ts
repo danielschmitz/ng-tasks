88e4fe1852172d5f83f25a8456706582b11017f0
Daniel Schmitz <danieljfa@gmail.com>
2025-02-07 15:46:53 -0300

    cancel loading after error

--- src/app/categories/edit/edit-category.component.ts
+++ src/app/categories/edit/edit-category.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CoreModule } from '../../core.module';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoriesService, Category } from '../categories.service';
-import { firstValueFrom, Observable, switchMap } from 'rxjs';
+import { finalize, firstValueFrom, Observable, switchMap } from 'rxjs';
 import { FormCategoryComponent } from '../form/form-category.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -45,17 +45,15 @@ export class EditCategoryComponent {
   onBack() {
     this.router.navigate(['/categories']);
   }
-  onSubmit(category: Category) {
+  async onSubmit(category: Category) {
     this.loading = true;
-    this.service
-      .updateCategory(category)
-      .subscribe({
-        next: () => {
-          this.router.navigate(['/categories']);
-        },
-        error: (error) => {
-          this.snak.open(error.error.message);
-        },
-      });
+    try {
+      await firstValueFrom(this.service.updateCategory(category));
+      this.router.navigate(['/categories']);
+    } catch (error: HttpErrorResponse | any) {
+      this.snak.open(error.error.message);
+    } finally {
+      this.loading = false;
+    }
   }
 }
